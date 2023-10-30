b8b53389a44f457a431ca3f3e7e24edf3fb2f6f9
Pedro Lucas Pedro <pedro.lucs0089@gmail.com>
2023-10-30 00:24:33 -0300

    .

---
+++ web/src/context/AuthContext.tsx
@@ -0,0 +1,19 @@
+// import { api } from "@/api/api";
+// import { ReactNode, createContext, useEffect } from "react";
+
+// interface AuthContextProviderChildren{
+//   children: ReactNode;
+// }
+
+// export const AuthContext = createContext();
+
+// export function AuthContextProvider({ children }: AuthContextProviderChildren){
+
+  
+
+//   return(
+//     <AuthContext.Provider value={}>
+//       {children}
+//     </AuthContext.Provider>
+//   )
+// }
---
+++ web/src/utils/userTypes.ts
@@ -0,0 +1,11 @@
+export interface UserTypes{
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  numberPhone: string;
+  bio: string;
+  profileUrl: string;
+  socials: string;
+  createAt: string;
+}
